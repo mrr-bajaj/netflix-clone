afe770f42c9891fc2bef80ed74d121eaeaf946a6
Shubham Bajaj <mrrbajaj44@gmail.com>
2024-02-10 20:27:21 +0530

    Trailer Title && Main container

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies"
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header></Header>
+      <MainContainer></MainContainer>
+      <SecondaryContainer></SecondaryContainer>
+      {/* 
+      
+        MainContainer
+         - Video Background
+         - Video Title
+        SecondaryContainer
+          - movies list * n
+            - card * n
+      */}
     </div>
   );
 };
