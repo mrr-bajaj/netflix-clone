40b17676c9e07b2ee202f186f27adb0a9406bf34
Shubham Bajaj <mrrbajaj44@gmail.com>
2024-03-06 02:49:00 +0530

    update path

--- src/components/MobileMovieCard.js
+++ src/components/MobileMovieCard.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { IMG_CDN_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { updatePath } from "../utils/configSlice";
 
 const MobileMovieCard = ({ movieInfo }) => {
+  const dispatch = useDispatch();
   return (
-    <div className="min-w-32 mx-2">
+    <div
+      className="min-w-32 mx-2"
+      onClick={() => {
+        dispatch(updatePath(movieInfo.id));
+      }}
+    >
       <Link to={"?jbv=" + movieInfo.id}>
         <img
           className="aspect-3/4"
